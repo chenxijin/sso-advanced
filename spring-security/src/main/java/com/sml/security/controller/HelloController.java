2e1796c8699ef41fcad4a25cb891aed1e78f54bc
chenxijin <a1501580300@qq.com>
2023-02-06 16:26:02 +0800

    keycloak Getting Started

--- spring-security/src/main/java/com/sml/security/controller/HelloController.java
+++ spring-security/src/main/java/com/sml/security/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello security";
     }
 }
